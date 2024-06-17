3bc2f1f519b84b8725737b688ec4f2d99a871458
Jasper Mayone <me@jaspermayone.com>
2024-06-17 12:37:50 -0400

    anylitics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,6 +9,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script defer src="https://umami.purplebubble.org/script.js" data-website-id="dcf09d4f-b160-4686-b316-66caa38ffb89"/>
         <SpeedInsights />
       </body>
     </Html>
