2024120363d9d621022dc2b5badafe3b1040fde2
Jasper Mayone <me@jaspermayone.com>
2024-07-18 10:53:47 -0400

    add mastodon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <Component {...pageProps} />
       <Script src="https://umami.jaspermayone.com/script.js" data-website-id={process.env.UMAMI_PROJECT_ID} />
+      <a rel="me" href="https://mastodon.social/@jaspermayone">Mastodon</a>
     </>
   )
 }
