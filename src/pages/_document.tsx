375c790f5de17c10d96cbe8a163fd2204f5c6e91
Jasper Mayone <me@jaspermayone.com>
2024-08-07 09:00:42 -0400

    Add csp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 export default function Document() {
   return (
     <Html lang="en">
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="default-src 'self'; img-src https://*; child-src 'none';"
+      />
       <Head />
       <body>
         <Main />
