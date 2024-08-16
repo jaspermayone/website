243d9a38e74f1b5680bc556bc62e3666a8e1cc46
Jasper Mayone <me@jaspermayone.com>
2024-08-16 11:21:18 -0400

    bugfixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <meta
-        http-equiv="Content-Security-Policy"
-        content="default-src 'self'; img-src https://*; child-src 'none';"
-      />
-      <meta
-        http-equiv="Strict-Transport-Security"
-        content="max-age=63072000; includeSubDomains; preload"
-      />
-      <meta http-equiv="X-Frame-Options" content="DENY" />
-      <meta http-equiv="X-Content-Type-Options" content="nosniff" />
-      <meta name="referrer" content="strict-origin"/>
       <Head />
       <body>
         <Main />
