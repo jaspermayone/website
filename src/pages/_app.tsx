98bbe499aa3ca8d3184035aecfef791a4d1de140
Jasper Mayone <me@jaspermayone.com>
2024-09-15 00:18:35 -0400

    Update src/pages/_app.tsx
    Co-authored-by: Aram <me@aram.sh>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <AnimatePresence>
       <Component {...pageProps} />
+      </AnimatePresence>
     </>
   );
 }
