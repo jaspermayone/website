d5cb28ff89b37e6c1b01c5ceae0e43648f5c1f0b
Aram <me@aram.sh>
2024-09-15 19:42:22 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,7 +56,7 @@ const config = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      }
+      },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
