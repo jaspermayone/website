7e2ca054dae334ccf5063a85b6b98c6b8523657a
Jasper Mayone <me@jaspermayone.com>
2025-05-28 14:32:32 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -314,15 +314,15 @@ export default function RootLayout({
         <link
           rel="icon"
           type="image/png"
-          href="/favicon-96x96.png"
+          href="/favicon-96x96.png?v=2"
           sizes="96x96"
         />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/apple-touch-icon.png?v=2"
         />
 
         {redirects.map((redirect) => (
