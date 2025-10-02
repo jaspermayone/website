df8e09d832a300362e8a583109e31cc784a04494
Jasper Mayone <me@jaspermayone.com>
2025-10-02 08:19:10 -0400

    dep some stuff

--- src/lib/defs.ts
+++ src/lib/defs.ts
@@ -6,7 +6,7 @@ import {
   SiThreads,
   SiX,
 } from "react-icons/si";
-import { Project, RedirectItem } from "./types";
+import { RedirectItem } from "./types";
 
 export const pages = [
   "home",
@@ -145,46 +145,6 @@ export const redirects: RedirectItem[] = [
     destination: "https://cv.jaspermayone.com",
     linkrelme: true,
     slashToLink: true,
-  },
-];
-
-export const projects: Project[] = [
-  {
-    title: "IT Tool",
-    description:
-      "Managing loaner chromebooks for students and teachers in the HUUSD school district.",
-    image: "/images/projects/ittool.png",
-    link: "https://github.com/jaspermayone/ittool",
-    techStack: ["Rails", "Ruby", "CSS", "JavaScript"],
-  },
-  {
-    title: "@jaspermayone/logger",
-    description: "Node.js Logging Package.",
-    image: "/images/projects/logger.png",
-    link: "https://github.com/jaspermayone/logger",
-    techStack: ["Node.js", "Typescript"],
-  },
-  {
-    title: "Phish Directory",
-    description:
-      "API for phish.directory, a community-driven anti-phishing tool. Helping catch, prevent, and catalog phishing links & attempts.",
-    image: "/images/projects/phish-directory.png",
-    link: "https://github.com/phish-directory/api",
-    techStack: ["Node.js", "Express", "Postgres", "Typescript", "Prisma"],
-  },
-  {
-    title: "Magic Mirror",
-    description: "Mirroring Slack Channels Between Workspaces",
-    image: "/images/projects/magic-mirror.jpg",
-    link: "https://github.com/thepurplebubble/magic-mirror",
-    techStack: [
-      "Node.js",
-      "@slack/bolt",
-      "Express",
-      "Typescript",
-      "Bun",
-      "Prisma",
-    ],
   },
 ];
 
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,3 @@
-export type Project = {
-  title: string;
-  description: string;
-  image: string;
-  link: string;
-  techStack: string[];
-};
-
 export interface ExperienceItem {
   company: string;
   location: string;
