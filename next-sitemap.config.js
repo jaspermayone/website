6b5562e98c86514accbb9d38402a9c371fe83689
Jasper Mayone <me@jaspermayone.com>
2025-12-01 15:18:33 -0500

    fix cannonical page issue

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://jaspermayone.com",
+  siteUrl: process.env.SITE_URL || "https://www.jaspermayone.com",
   generateRobotsTxt: true,
 };
