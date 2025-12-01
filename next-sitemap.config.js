c61255ccb3a873317ab265e146561fa5c726f0be
Jasper Mayone <me@jaspermayone.com>
2025-12-01 15:44:55 -0500

    fix sitemap and also move to next robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.jaspermayone.com",
-  generateRobotsTxt: true,
+  generateRobotsTxt: false, // Using Next.js native robots.ts instead
+  exclude: ["/icon.png", "/robots.txt", "/blank", "/api/*"],
 };
