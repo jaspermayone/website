fa82f53e1678cde096320fc4011a363ddf501761
Jasper Mayone <me@jaspermayone.com>
2025-12-01 16:03:36 -0500

    this should fix the sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,6 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.jaspermayone.com",
   generateRobotsTxt: false, // Using Next.js native robots.ts instead
   generateIndexSitemap: true,
-  sitemapBaseFileName: "sitemap_index",
   exclude: ["/icon.png", "/robots.txt", "/blank", "/api/*"],
   priority: 0.7,
   changefreq: "weekly",
