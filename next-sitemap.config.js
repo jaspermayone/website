4ab28f066422c4f260e74af7eff1b4558010a3d4
Jasper Mayone <me@jaspermayone.com>
2025-12-01 15:58:41 -0500

    sitemap convention

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.jaspermayone.com",
   generateRobotsTxt: false, // Using Next.js native robots.ts instead
+  generateIndexSitemap: true,
+  sitemapBaseFileName: "sitemap_index",
   exclude: ["/icon.png", "/robots.txt", "/blank", "/api/*"],
   priority: 0.7,
   changefreq: "weekly",
